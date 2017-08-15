ed004dc03be7865753b601f88efce322876eba80
Andre Foeken <andre.foeken@nedap.com>
2017-08-15 20:10:35 +0200

    Added test case for UUID relationships queries

--- test/integration/mysql_integration_test.rb
+++ test/integration/mysql_integration_test.rb
@@ -6,6 +6,14 @@ require_relative '../test_helper'
 
 class MyUuidModel < ActiveRecord::Base
   attribute :the_uuid, MySQLBinUUID::Type.new
+
+  has_many :relationships, class_name: "MyUuidRelationship"
+end
+
+class MyUuidRelationship < ActiveRecord::Base
+  attribute :the_uuid, MySQLBinUUID::Type.new
+
+  belongs_to :model, class_name: "MyUuidModel"
 end
 
 describe MyUuidModel do
@@ -35,25 +43,31 @@ describe MyUuidModel do
     ActiveRecord::Base.establish_connection(db_config)
     connection.create_table("my_uuid_models")
     connection.add_column("my_uuid_models", "the_uuid", :binary, limit: 16)
+
+    connection.create_table("my_uuid_relationships")
+    connection.add_column("my_uuid_relationships", "the_uuid", :binary, limit: 16)
+    connection.add_column("my_uuid_relationships", "my_uuid_model_id", :integer)
   end
 
   after(:all) do
     connection.drop_database(db_config[:database])
   end
 
-  let(:sample_uuid) { SecureRandom.uuid }
+  let(:model_uuid) { SecureRandom.uuid }
+  let(:relationship_uuid) { SecureRandom.uuid }
 
   context "without saving" do
     it "does not change the uuid when retrieved without saving" do
-      my_model = MyUuidModel.new(the_uuid: sample_uuid)
-      assert_equal sample_uuid, my_model.the_uuid
-      assert_equal sample_uuid, my_model.attributes["the_uuid"]
+      my_model = MyUuidModel.new(the_uuid: model_uuid)
+      assert_equal model_uuid, my_model.the_uuid
+      assert_equal model_uuid, my_model.attributes["the_uuid"]
     end
   end
 
   context "after persisting to the database" do
     before do
-      @my_model = MyUuidModel.create!(the_uuid: sample_uuid)
+      @my_model = MyUuidModel.create!(the_uuid: model_uuid)
+      @my_relationship = MyUuidRelationship.create!(the_uuid: SecureRandom.uuid, my_uuid_model_id: @my_model)
     end
 
     after do
@@ -69,24 +83,35 @@ describe MyUuidModel do
       raw_value = connection.execute("SELECT * FROM my_uuid_models").to_a.first[1]
 
       # Create a version without dashes of the sample uuid
-      sample_uuid_no_dashes = sample_uuid.delete("-")
+      model_uuid_no_dashes = model_uuid.delete("-")
 
       # Put it in an array so we can create the binary representation we
       # also get from the database.
-      assert_equal [sample_uuid_no_dashes].pack("H*"), raw_value
+      assert_equal [model_uuid_no_dashes].pack("H*"), raw_value
     end
 
     it "can be found using .find_by" do
-      find_result = MyUuidModel.find_by(the_uuid: sample_uuid)
+      find_result = MyUuidModel.find_by(the_uuid: model_uuid)
       assert_equal find_result, @my_model
-      assert_equal find_result.the_uuid, sample_uuid
+      assert_equal find_result.the_uuid, model_uuid
     end
 
     it "can be found using .where" do
-      results = MyUuidModel.where(the_uuid: sample_uuid)
+      results = MyUuidModel.where(the_uuid: model_uuid)
       assert_equal results.count, 1
       assert_equal results.first, @my_model
-      assert_equal results.first.the_uuid, sample_uuid
+      assert_equal results.first.the_uuid, model_uuid
+    end
+
+    it "relationships can be found using .where" do
+      results = MyUuidModel.joins(:relationships).where(my_uuid_relationships: {the_uuid: relationship_uuid})
+
+      # FOR DEBUG: Here can be seen the UUID column is not converted to binary.
+      puts MyUuidModel.joins(:relationships).where(my_uuid_relationships: {the_uuid: relationship_uuid}).to_sql
+
+      assert_equal results.count, 1
+      assert_equal results.first, @my_relationship
+      assert_equal results.first.the_uuid, relationship_uuid
     end
   end
 
