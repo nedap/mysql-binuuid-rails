819f3c1917cde824d16a446b5b9e437032ff7c57
Tjalling van der Wal <tjalling.vanderwal@nedap.com>
2019-06-27 10:13:46 +0200

    ActiveModel is a dependency of ActiveRecord

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,5 @@ require 'minitest/pride'
 
 require 'minitest-spec-context'
 
-require 'active_model/type'
-require 'active_model/type/value'
-require 'active_model/type/binary'
-
 require_relative '../lib/mysql-binuuid-rails'
 require_relative '../lib/mysql-binuuid/type'
